e70d47709b38dc0dbf6cfddafb594bc6cc46cd08
Ollie <reelfeet@hotmail.com>
2019-06-30 09:12:55 +0100

    Updates

---
+++ precache-manifest.810fb7a5c1535adc37eb28acf7b76174.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a0910837be71e0fafd1765acfb2e46",
+    "url": "/sum-fun/index.html"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/sum-fun/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "201b0afb042f8e755096",
+    "url": "/sum-fun/static/js/main.81ec2bf7.chunk.js"
+  },
+  {
+    "revision": "78cfa4734442638de5cf",
+    "url": "/sum-fun/static/js/runtime~main.4ff58985.js"
+  }
+]);
